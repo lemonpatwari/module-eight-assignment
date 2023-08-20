79b63e68ba4ebe74bdaed4e242ea861eda0e15cb
Lemon Patwari <lemonpatwari@gmail.com>
2023-08-20 23:52:54 +0600

    emit add

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import {defineProps, defineEmits} from 'vue';
+defineProps(['message'])
+
+const emit = defineEmits(['messageEmitted'])
+
+const emitMessage = () => {
+  emit('messageEmitted', 'Hello from ChildComponent');
+};
+
+</script>
+
+<template>
+  <h1>ChildComponent</h1>
+  <p class="text-yellow-600">{{ message }}</p>
+
+  <button @click="emitMessage" class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-8 py-2 px-4 rounded">
+    Button
+  </button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import ChildComponent from "./ChildComponent.vue";
+import {ref} from "vue";
+
+const message = ref('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, quia.');
+
+const emittedMessage = ref('');
+
+const handleMessage = (message) => {
+  console.log(message);
+  emittedMessage.value = message;
+};
+
+</script>
+
+<template>
+  <h1>ParentComponent</h1>
+  <h1>{{ emittedMessage }}</h1>
+  <ChildComponent @messageEmitted="handleMessage" :message="message"/>
+</template>
+
+<style scoped>
+
+</style>
